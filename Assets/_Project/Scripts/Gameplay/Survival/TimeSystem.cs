6ece1ee5e8561fe8a2be9aaf5e5163972d23de67
pandamorda <109853076+pandamorda@users.noreply.github.com>
2026-04-02 17:02:42 +0300

    feat: TimeSystem HDRP - sun/moon lighting, day-night cycle

--- Assets/_Project/Scripts/Gameplay/Survival/TimeSystem.cs
+++ Assets/_Project/Scripts/Gameplay/Survival/TimeSystem.cs
@@ -1,10 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class TimeSystem : MonoBehaviour
-{
+{ 
     [SerializeField] private Light sun;
-    [SerializeField] private AnimationCurve lightIntensityCurve;
+    private HDAdditionalLightData sunHD;
+    [SerializeField] private Volume globalVolume;
+    [SerializeField] private AnimationCurve luxCurve;
+    [SerializeField] private AnimationCurve colorTemperatureCurve;
+    [SerializeField] private AnimationCurve temperatureCurve;
+    [SerializeField] private Light moon;
+    private HDAdditionalLightData moonHD;
     
     [SerializeField] private float dayDuration;
     [SerializeField] private float minTemperature;
@@ -17,16 +25,63 @@ public class TimeSystem : MonoBehaviour
     public int DayCount => dayCount;
     public float TimeNormalized => time;
 
+    private void Awake()
+    {
+        sunHD = sun.GetComponent<HDAdditionalLightData>();
+        moonHD = moon.GetComponent<HDAdditionalLightData>();
+    }
+
     private void Update()
     {
         time += Time.deltaTime / dayDuration;
-        sun.transform.rotation = Quaternion.Euler(time * 360f - 90f, 170f, 0f);
-        sun.intensity = lightIntensityCurve.Evaluate(time*100000);
+        
+        UpdateSunRotation();
+        UpdateSunLight();
+        UpdateTemperature();
+        UpdateVolume();
+        UpdateMoonLight();
+        
         
         if (time >= 1)
         {
             time -= 1;
             dayCount++;
         }
+    }
+    private void UpdateMoonLight()
+    {
+        float moonIntensity = 1f - luxCurve.Evaluate(time) / 100000f;
+        moonHD.SetIntensity(Mathf.Clamp(moonIntensity * 500f, 0f, 500f), LightUnit.Lux);
+        moon.transform.rotation = Quaternion.Euler(time * 360f + 90f, 170f, 0f);
+    }
+    void UpdateSunRotation()
+    {
+        sun.transform.rotation = Quaternion.Euler(time * 360f - 90f, 170f, 0f);
+    }
+
+    void UpdateSunLight()
+    {
+        sunHD.SetIntensity(luxCurve.Evaluate(time), LightUnit.Lux);
+        sunHD.SetColor(Color.white, colorTemperatureCurve.Evaluate(time));
+    }
+
+    void UpdateVolume()
+    {
+        float t = luxCurve.Evaluate(time) / 100000f;
+
+        if (globalVolume.profile.TryGet<Exposure>(out var exposure))
+        {
+            exposure.mode.value = ExposureMode.Fixed;
+            exposure.fixedExposure.value = Mathf.Lerp(8f, 13f, t); 
+        }
+        if (globalVolume.profile.TryGet<HDRISky>(out var sky))
+        {
+            sky.exposure.value = Mathf.Lerp(2f, 12f, t); 
+        }
+    }
+
+    void UpdateTemperature()
+    {
+        CurrentTemperature = Mathf.Lerp(minTemperature, maxTemperature, temperatureCurve.Evaluate(time));
     }
 }
