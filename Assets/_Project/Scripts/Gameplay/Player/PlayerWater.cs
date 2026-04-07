d0f9a79032aff4ffe234a161277bebcb55d8756b
pandamorda <109853076+pandamorda@users.noreply.github.com>
2026-04-07 16:44:21 +0300

    refactor: remove wave simulation logic from PlayerWater, use HDRP Water Surface

--- Assets/_Project/Scripts/Gameplay/Player/PlayerWater.cs
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerWater.cs
@@ -4,45 +4,18 @@ namespace _Project.Scripts.Gameplay.Player
 {
     public class PlayerWater : MonoBehaviour
     {
-        private PlayerRoot root;
+       
         private bool inWater;
 
 
         private float waterSurfaceY;
 
-
-        [Header("Wave Sync (must match WaterGraph shader)")] [SerializeField]
-        private float waveHeight1 = 0.2f;
-
-        [SerializeField] private float waveFrequency1 = 2f;
-        [SerializeField] private float waveSpeed1 = 1f;
-        [SerializeField] private float waveHeight2 = 0.1f;
-        [SerializeField] private float waveFrequency2 = 1.5f;
-        [SerializeField] private float waveSpeed2 = 1f;
-
-
-        public float WaterSurfaceY
-        {
-            get
-            {
-                float x = transform.position.x;
-                float z = transform.position.z;
-                float wave1 = Mathf.Sin(x * waveFrequency1 + Time.time * waveSpeed1) * waveHeight1;
-                float wave2 = Mathf.Cos(z * waveFrequency2 + Time.time * waveSpeed2) * waveHeight2;
-                return waterSurfaceY + wave1 + wave2;
-            }
-        }
-
-       
-        public float SubmersionDepth => WaterSurfaceY - transform.position.y;
-
+        public float WaterSurfaceY => waterSurfaceY;
+        
         
         public bool InWater => inWater;
 
-        void Awake()
-        {
-            root = GetComponent<PlayerRoot>();
-        }
+       
 
         public void EnterWater(float surfaceY)
         {
