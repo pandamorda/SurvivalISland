553ccb862a4b38c6fa2e82e6ef8b6c2e605969c6
pandamorda <109853076+pandamorda@users.noreply.github.com>
2026-05-19 17:05:41 +0300

    fix: gate behavior.Tick by _isInteracting flag

--- Assets/_Project/Scripts/Gameplay/Interaction/Core/InteractableBase.cs
+++ Assets/_Project/Scripts/Gameplay/Interaction/Core/InteractableBase.cs
@@ -7,10 +7,12 @@ namespace _Project.Scripts.Gameplay.Interaction
         public bool CanInteract => true;
         [SerializeField] private InteractKey key;
         [SerializeReference, SubclassSelector] private IExtractionBehavior behavior;
+        
         public InteractKey Key => key;
         private Color originalColor;
         [SerializeField] private Color focusColor = Color.green;
         private MeshRenderer _renderer;
+        private bool _isInteracting;
         void Awake()
         {
             _renderer = GetComponent<MeshRenderer>();
@@ -32,19 +34,23 @@ namespace _Project.Scripts.Gameplay.Interaction
         public void StartInteract()
         {
             behavior.Begin(this);
+            _isInteracting = true;
         }
 
         public void StopInteract()
         {
             behavior.OnInputReleased(this);
+            _isInteracting = false;
         }
 
         public void Tick(float deltaTime)
         {
+            if (!_isInteracting) return;
             ExtractionTickResult result = behavior.Tick(deltaTime);
             if (result.Status == ExtractionStatus.Completed)
             {
                 behavior.Complete(this);
+                _isInteracting = false;
             }
         }
     }
